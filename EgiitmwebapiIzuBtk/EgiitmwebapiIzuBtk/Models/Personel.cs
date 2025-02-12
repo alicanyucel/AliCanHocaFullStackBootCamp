6e514151af218cea72b866101abf81024eaa7e80
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2025-02-12 17:56:53 +0300

    WebApi Crud Dersi Eklendi.

---
+++ EgiitmwebapiIzuBtk/EgiitmwebapiIzuBtk/Models/Personel.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace EgiitmwebapiIzuBtk.Models
+{
+    public sealed class Personel
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName {  get; set; }
+        
+    }
+}
